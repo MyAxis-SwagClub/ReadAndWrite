9049fef7fe6d7a425a6022f144b25265d6725308
MyAxis <timon03032007@gmail.com>
2026-05-20 22:00:23 +0300

    Создали кроссбар приложения для взаимодествия с окнами

--- ReadAndWrite/MainWindow.xaml.cs
+++ ReadAndWrite/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using ReadAndWrite.pages;
 using ReadAndWrite.Pages;
 
 namespace ReadAndWrite
@@ -8,7 +9,31 @@ namespace ReadAndWrite
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new LoginPage());
+            SetupSidebar();
+            MainFrame.Navigate(new CatalogPage());
         }
+
+        public void SetupSidebar()
+        {
+            BtnAdmin.Visibility = CurrentUser.IsAdmin
+                ? Visibility.Visible : Visibility.Collapsed;
+            BtnAuthor.Visibility = CurrentUser.IsAuthor
+                ? Visibility.Visible : Visibility.Collapsed;
+            BtnFrozen.Visibility = CurrentUser.IsFrozen
+                ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void BtnCatalog_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new CatalogPage());
+        private void BtnLists_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ReadingListPage());
+        private void BtnProfile_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ProfilePage());
+        private void BtnAuthor_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new AuthorPage());
+        private void BtnAdmin_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new AdminPage());
+        private void BtnFrozen_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ProfilePage());
     }
 }
