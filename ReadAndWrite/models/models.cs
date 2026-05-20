522abe4d46b65f31ec4b4c507ea58e18ae4468d9
MyAxis <timon03032007@gmail.com>
2026-05-20 10:22:19 +0300

    Создан класс Models

--- ReadAndWrite/models/models.cs
+++ ReadAndWrite/models/models.cs
@@ -11,7 +11,7 @@ namespace ReadAndWrite.Models
         public int BookId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string CoverPath { get; set; }
+        public string CoverPath { get; set; } 
         public string Content { get; set; }
         public int AuthorId { get; set; }
         public string AuthorName { get; set; }
