3d1fcb58db630eca12e6f6e6b931167d566ce7cc
MyAxis <timon03032007@gmail.com>
2026-05-20 09:47:41 +0300

    Добавьте файлы проекта.

---
+++ ReadAndWrite/DataBaseHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ReadAndWrite
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string ConnectionString =
+            "Server=localhost;Database=ReadAndWrite;Trusted_Connection=True;" +
+            "TrustServerCertificate=True;";
+        public static DataTable ExecuteQuery(string query, SqlParameter[] parameters = null)
+        {
+            var table = new DataTable();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                connection.Open();
+                using (var adapter = new SqlDataAdapter(command))
+                    adapter.Fill(table);
+            }
+            return table;
+        }
+        public static int ExecuteNonQuery(string query, SqlParameter[] parameters = null)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                connection.Open();
+                return command.ExecuteNonQuery();
+            }
+        }
+        public static object ExecuteScalar(string query, SqlParameter[] parameters = null)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                    command.Parameters.AddRange(parameters);
+
+                connection.Open();
+                return command.ExecuteScalar();
+            }
+        }
+    }
+}
